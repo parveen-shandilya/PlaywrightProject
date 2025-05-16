d3df7a49ebf21f6ed26310c64a88bc0d831fd6d1
parveen-shandilya <parveenritwal5362653@gmail.com>
2025-05-16 11:25:53 +0530

    playwright added

--- tests/codeGen.spec.ts
+++ tests/codeGen.spec.ts
@@ -7,3 +7,25 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Search').fill('playwright');
   await page.getByPlaceholder('Search').press('Enter');
 });
+
+test('test1', async ({ page }) => {
+  await page.goto('https://www.guru99.com/');
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByLabel('Primary Navigation').getByRole('link', { name: 'Testing' }).click();
+  await page.getByRole('link', { name: 'Software Testing', exact: true }).click();
+  await page.getByTitle('What is Software Testing?').click();
+  await page.goto('https://www.guru99.com/software-testing-introduction-importance.html');
+  await page.getByRole('link', { name: 'Next' }).click();
+});
+
+test.only('test2', async ({ page }) => {
+  await page.goto('https://www.guru99.com/');
+  await page.getByLabel('Primary Navigation').getByRole('link', { name: 'Testing' }).click();
+  await page.getByRole('link', { name: 'SAP Testing' }).click();
+  await page.getByRole('link', { name: 'What is Maintenance of SAP' }).click();
+  await page.getByRole('link', { name: 'Types of Testing Applicable to SAP Applications', exact: true }).click();
+  await page.getByRole('link', { name: 'Big Data' }).click();
+  await page.getByRole('link', { name: 'DevOps' }).click();
+  await page.getByRole('link', { name: 'Puppet Tutorial for Beginners' }).click();
+});
+
